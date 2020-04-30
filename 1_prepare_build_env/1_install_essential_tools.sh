eb26e1b9e7284e297b89a55669f6b6497f44cb71
orex <orex@orrecx.com>
2020-04-30 12:58:38 +0200

    error fixed

--- 1_prepare_build_env/1_install_essential_tools.sh
+++ 1_prepare_build_env/1_install_essential_tools.sh
@@ -5,7 +5,7 @@ function install_all_needed_tools ()
   apt-get update
   echo "installing needed tools"
   apt-get install -y bash grep gzip binutils-common bison bzip2 coreutils diffutils findutils gawk \
-  m4 patch perl python3 sed tar texinfo xz build-essentials
+  m4 patch perl python3 sed tar texinfo xz-utils build-essential wget
 }
 
 function check_tools_version ()
