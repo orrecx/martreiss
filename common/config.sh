2578180ee86e80e81f53a72e4b6a9d7f2e125065
orex <orex@orrecx.com>
2020-05-02 20:18:22 +0200

    clean code

--- common/config.sh
+++ common/config.sh
@@ -6,35 +6,30 @@ if [ -z "$LFS" ]; then
     else
             LFS="/lfs"
     fi
-    export LFS
 fi
 
 if [ -z "$LFS_TGT" ]; then
     LFS_TGT="$(uname -m)-lfs-linux-gnu"
-    export LFS_TGT
 fi
 
 if [ -z "$SOURCES_DIR" ]; then
     SOURCES_DIR="$LFS/sources"
-    export SOURCES_DIR
 fi
 
 if [ -z "$TOOLS_SLINK" ]; then
     TOOLS_SLINK="/tools"
-    export TOOLS_DIR
 fi
 
 if [ -z "$TOOLS_DIR" ]; then
     TOOLS_DIR="${LFS}${TOOLS_SLINK}"
-    export TOOLS_DIR
 fi
 
 if [ -z "$COMPONENTS_DIR" ]; then
     COMPONENTS_DIR="components"
-    export COMPONENTS_DIR
 fi
 
 if [ -z "$UTILS" ]; then
     UTILS="common"
-    export UTILS
 fi
+
+export LFS LFS_TGT SOURCES_DIR TOOLS_SLINK TOOLS_DIR COMPONENTS_DIR UTILS
